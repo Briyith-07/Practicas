0f06bf89cc797f23a55c8cb89a7d21eb6188007e
Briyith07 <bcjc3170@gmail.com>
2025-08-06 11:12:50 -0500

    Migrar proyecto a siso-tool sin subrepositorio

--- sst/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.inicio, name='inicio'),
-    path('login/', views.login_view, name='login'),
-    path('register/', views.register_view, name='register'),
-    path('actividades/', views.lista_actividades, name='lista_actividades'),
-]
